5dfc0421bb8d3e559091c39e3630afce0c8eede3
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2020-11-16 23:53:23 +0100

    Add files via upload

--- ex1/src/Optimization/Loss.py
+++ ex1/src/Optimization/Loss.py
@@ -7,9 +7,9 @@ class CrossEntropyLoss():
     def forward(self,input_tensor,label_tensor):#here input = predicted y//////label tensor is composed of 0 and 1
         self.input_tensor = input_tensor
         eps = np.finfo(float).eps
-        loss_elementwise = -np.log(input_tensor+eps)*label_tensor
+        loss_elementwise = -np.log(input_tensor+eps)*label_tensor#elementwisely calculate log,it's conditioned with yk = 1,so elementwisely multiiply label_tensor
         loss_all = np.sum(loss_elementwise)
         return loss_all
 
     def backward(self,label_tensor):
-        return (-np.true_divide(label_tensor,self.input_tensor))
+        return (-np.true_divide(label_tensor,self.input_tensor))#En = - y/y^ ,y^is between 0 1, if directly divide, only int part,we need float part too
