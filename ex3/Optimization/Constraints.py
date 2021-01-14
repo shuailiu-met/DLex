cdbf49ccf92e3fd0d553e63ed1c7e7d568acd1a3
shuailiu-met <58299241+shuailiu-met@users.noreply.github.com>
2021-01-14 08:27:53 +0100

    Add files via upload

--- ex3/Optimization/Constraints.py
+++ ex3/Optimization/Constraints.py
@@ -1,27 +1,26 @@
-import numpy as np
-import math
-
-
-
+import numpy as np 
 
 class L2_Regularizer():
-    def __init__(self, alpha):
+    def __init__(self,alpha):
         self.alpha = alpha
-        self.regularizer = None
 
-    def calculate_gradient(self, weights):
-        return self.alpha * weights
+    def calculate_gradient(self,weights):
+        adjust = self.alpha*weights
+        return adjust
 
-    def norm(self, weights):
-        return math.pow(np.linalg.norm(weights), 2) * self.alpha
+    def norm(self,weights):
+        norml2 = self.alpha*np.square(np.sqrt(np.sum(np.power(np.abs(weights),2)))) ### Lhat = L + square of L2(w)
+        return norml2
 
 
 class L1_Regularizer():
-    def __init__(self, alpha):
+    def __init__(self,alpha):
         self.alpha = alpha
 
-    def calculate_gradient(self, weights):
-        return self.alpha * np.sign(weights)
+    def calculate_gradient(self,weights):
+        adjust = self.alpha*np.sign(weights)
+        return adjust
 
-    def norm(self, weights):
-        return np.sum(np.abs(weights)) * self.alpha
+    def norm(self,weights):
+        norml1 = self.alpha*np.sum(np.abs(weights))
+        return norml1
--- ex3/Optimization/Optimizers.py
+++ ex3/Optimization/Optimizers.py
@@ -1,27 +1,30 @@
 import numpy as np
-import copy
 
-class Optimizer:
-    def __int__(self):
+class Optimizer():
+    def __init__(self):
         self.regularizer = None
-        self.update_resulte = None
 
-    def add_regularizer(self, regularizer):
-        self.regularizer = copy.deepcopy(regularizer)
-
+    def add_regularizer(self,regularizer):
+        self.regularizer = regularizer
 
 class Sgd(Optimizer):
-    def __init__(self, learning_rate):
+    def __init__(self,learning_rate):
+        Optimizer.__init__(self)
         self.learning_rate = learning_rate
         #print(self.learning_rate)
 
     def calculate_update(self,weight_tensor,gradient_tensor):
-        return weight_tensor - self.learning_rate * self.regularizer.calculate_gradient(weight_tensor) - self.learning_rate*gradient_tensor #in practice, gradient_tensor is the gradient of weights,which will update the weight
+
+        updated_weight = weight_tensor - self.learning_rate*gradient_tensor #in practice, gradient_tensor is the gradient of weights,which will update the weight
+        if self.regularizer is not None:
+            updated_weight -= self.learning_rate*self.regularizer.calculate_gradient(weight_tensor)   # adjust for regularization
+        return updated_weight
 
 
 
 class SgdWithMomentum(Optimizer):
     def __init__(self,learning_rate,momentum_rate):
+        Optimizer.__init__(self)
         self.learning_rate = learning_rate
         self.momentum_rate = momentum_rate
         self.vk = 0#vk starts with 0
@@ -29,18 +32,23 @@ class SgdWithMomentum(Optimizer):
     def calculate_update(self,weight_tensor,gradient_tensor):# Vk needs to be updated and stored for the next use
         vk_now = self.momentum_rate*self.vk - self.learning_rate*gradient_tensor
         self.vk = vk_now
-        return weight_tensor - self.learning_rate * self.regularizer.calculate_gradient(weight_tensor) +vk_now
+        updated_weight = weight_tensor+vk_now
+        if self.regularizer is not None:
+            updated_weight -= self.learning_rate*self.regularizer.calculate_gradient(weight_tensor) # adjust for regularization
+        return updated_weight
 
 
 
 class Adam(Optimizer):
     def __init__(self,learning_rate,mu,rho):
+        Optimizer.__init__(self)
         self.learning_rate = learning_rate
         self.mu = mu
         self.rho = rho
         self.vk = 0
         self.rk = 0   #we need vk / rk to be stored for further usage
         self.k = 1    #the int number for power part mu^k and rho^k,because we have k-1 cases, k starts with 1
+        # vk,rk is usually initialized with 0
 
     def calculate_update(self,weight_tensor,gradient_tensor):
         gk = gradient_tensor
@@ -56,5 +64,9 @@ class Adam(Optimizer):
         self.k = self.k+1  # update k idx
 
         eps = np.finfo(float).eps
-        weights_next = weight_tensor - self.learning_rate*(vk_hat/(np.sqrt(rk_hat)+eps))
-        return weights_next - self.learning_rate * self.regularizer.calculate_gradient(weight_tensor)
+        updated_weight = weight_tensor - self.learning_rate*(vk_hat/(np.sqrt(rk_hat)+eps))
+        if self.regularizer is not None:
+            updated_weight -= self.learning_rate*self.regularizer.calculate_gradient(weight_tensor)  # adjust for regularization
+        return updated_weight
+
+
